40944bef848722a751528e17a3e2b13c19ccc5d1
pearl0361 <pearl_seizetheday@naver.com>
2018-02-02 13:17:42 +0900

    어제 안올리고 간거 추가

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -48,13 +48,10 @@ public class MainController {
 		return mav;
 	}
 
-	// @RequestMapping(value="/pages/news.do")
 	@RequestMapping("news.do")
 	public ModelAndView news() {
 
 		ModelAndView mav = new ModelAndView();
-		// mav.setViewName("news");
-		// mav.setViewName("New");
 		return mav;
 	}
 
@@ -70,39 +67,3 @@ public class MainController {
 		return mav;
 	}
 }
-
-/*
- * package controller;
- * 
- * import java.io.IOException;
- * 
- * import javax.servlet.ServletException; import
- * javax.servlet.annotation.WebServlet; import javax.servlet.http.HttpServlet;
- * import javax.servlet.http.HttpServletRequest; import
- * javax.servlet.http.HttpServletResponse;
- * 
- * import org.springframework.stereotype.Controller; import
- * org.springframework.web.bind.annotation.RequestMapping;
- * 
- * @Controller
- * 
- * @WebServlet("/MainController") public class MainController extends
- * HttpServlet{
- * 
- * @Override protected void doGet(HttpServletRequest req, HttpServletResponse
- * resp) throws ServletException, IOException { // TODO Auto-generated method
- * stub System.out.println("서브릿 들어옴"); String url = "/WEB-INF/index.jsp"; //
- * String url = "/WEB-INF/pages/News.jsp";
- * req.getRequestDispatcher(url).forward(req, resp); }
- * 
- * @Override protected void doPost(HttpServletRequest req, HttpServletResponse
- * resp) throws ServletException, IOException { // TODO Auto-generated method
- * stub req.setCharacterEncoding("UTF-8"); doGet(req, resp); }
- * 
- * @RequestMapping("News.do") public String News() {
- * 
- * return "/pages/News"; }
- * 
- * // @RequestMapping("loginForm.do") // public String loginForm() { // return
- * "loginForm.tiles"; // } }
- */
