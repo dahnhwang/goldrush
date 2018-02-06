fd97a44a28f7ba38a1df68470e2c57c607bbf2f1
student <student@M110110>
2018-02-06 10:49:09 +0900

    trend keyword 서버 작업 -다현

--- src/dao/ITrendKeywordDao.java
+++ src/dao/ITrendKeywordDao.java
@@ -7,7 +7,8 @@ import model.TrendKeyword;
 
 public interface ITrendKeywordDao {
 
-	// 조회하고자 하는 년도를 입력하면 해당 년도에 해당하는 월별 키워드셋을 모두 가져옴
+	// 조회하고자 하는 기간을 입력하면 해당 기간에 해당하는 월단위 키워드셋을 모두 가져옴
+	// 모드 0일 경우 딱 선택한 기간만 보여줌(년단위), 모드 1일 경우 start, end 기간 모두 가져옴 
 	public List<TrendKeyword> selectTrendKeywordByYear(HashMap<String, Object> params);
 
 }
--- src/service/TrendKeywordService.java
+++ src/service/TrendKeywordService.java
@@ -2,11 +2,22 @@ package service;
 
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.ITrendKeywordDao;
+
+@Service
 public class TrendKeywordService implements ITrendKeywordService {
+
+	@Autowired
+	private ITrendKeywordDao keywordDao;
 
 	@Override
 	public HashMap<String, Object> getTrendKeywordList(String k_year) {
-		// TODO Auto-generated method stub
+		HashMap<String, Object> params = new HashMap<>();
+		params.put("period", k_year);
+
 		return null;
 	}
 
