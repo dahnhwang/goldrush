7e09866fb77e42aef08d28be03992aa7a8f49171
student <student@M110110>
2018-02-06 10:29:48 +0900

    Trend Keyword 매퍼 수정 - 다현

--- src/dao/ITrendKeywordDao.java
+++ src/dao/ITrendKeywordDao.java
@@ -1,12 +1,13 @@
 package dao;
 
 import java.util.HashMap;
+import java.util.List;
 
 import model.TrendKeyword;
 
 public interface ITrendKeywordDao {
 
 //	조회하고자 하는 년도를 입력하면 해당 년도에 해당하는 월별 키워드셋을 모두 가져옴
-	public TrendKeyword selectTrendKeywordByYear(HashMap k_year);
+	public List<TrendKeyword> selectTrendKeywordByYear(HashMap k_year);
 	
 }
