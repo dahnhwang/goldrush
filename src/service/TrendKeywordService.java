1286841abc9c73918486e277cb4fe6a041070d9a
student <student@M110110>
2018-02-06 10:56:36 +0900

    trend keyword 서비스단 작업 - 다현

--- src/service/TrendKeywordService.java
+++ src/service/TrendKeywordService.java
@@ -1,11 +1,13 @@
 package service;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dao.ITrendKeywordDao;
+import model.TrendKeyword;
 
 @Service
 public class TrendKeywordService implements ITrendKeywordService {
@@ -16,9 +18,12 @@ public class TrendKeywordService implements ITrendKeywordService {
 	@Override
 	public HashMap<String, Object> getTrendKeywordList(String k_year) {
 		HashMap<String, Object> params = new HashMap<>();
-		params.put("period", k_year);
+		params.put("k_year", k_year);
+		List<TrendKeyword> keywordList = keywordDao.selectTrendKeywordByYear(params);
 
-		return null;
+		HashMap<String, Object> results = new HashMap<>();
+		results.put("keywordList", keywordList);
+		return results;
 	}
 
 }
