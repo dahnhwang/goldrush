bfaf413427e49d3266912dd1d4daf8b32d50f2f7
student <student@M110110>
2018-02-06 10:25:38 +0900

    trend keyword 관련 dao, model 클래스 수정사항 반영 - 다현

--- src/dao/ITrendKeywordDao.java
+++ src/dao/ITrendKeywordDao.java
@@ -1,12 +1,12 @@
 package dao;
 
+import java.util.HashMap;
+
 import model.TrendKeyword;
 
 public interface ITrendKeywordDao {
 
-	public int insertTrendKeyword(TrendKeyword trendKeyword);
+//	조회하고자 하는 년도를 입력하면 해당 년도에 해당하는 월별 키워드셋을 모두 가져옴
+	public TrendKeyword selectTrendKeywordByYear(HashMap k_year);
 	
-	public int deleteTrendKeyword(int k_id);
-	
-	public TrendKeyword selectTrendKeyword(int k_id);
 }
--- src/model/TrendKeyword.java
+++ src/model/TrendKeyword.java
@@ -1,182 +1,122 @@
 package model;
 
-import java.util.Date;
-
 public class TrendKeyword {
 
 	private int k_id;
-	private Date k_month;
+	private String k_month;
 	private String keyword1;
 	private String keyword2;
 	private String keyword3;
 	private String keyword4;
 	private String keyword5;
-	private String keyword6;
-	private String keyword7;
-	private String keyword8;
-	private String keyword9;
-	private String keyword10;
 	private int keyword1_freq;
 	private int keyword2_freq;
 	private int keyword3_freq;
 	private int keyword4_freq;
 	private int keyword5_freq;
-	private int keyword6_freq;
-	private int keyword7_freq;
-	private int keyword8_freq;
-	private int keyword9_freq;
-	private int keyword10_freq;
-	private int k_total;
+
 	public int getK_id() {
 		return k_id;
 	}
+
 	public void setK_id(int k_id) {
 		this.k_id = k_id;
 	}
-	public Date getK_month() {
+
+	public String getK_month() {
 		return k_month;
 	}
-	public void setK_month(Date k_month) {
+
+	public void setK_month(String k_month) {
 		this.k_month = k_month;
 	}
+
 	public String getKeyword1() {
 		return keyword1;
 	}
+
 	public void setKeyword1(String keyword1) {
 		this.keyword1 = keyword1;
 	}
+
 	public String getKeyword2() {
 		return keyword2;
 	}
+
 	public void setKeyword2(String keyword2) {
 		this.keyword2 = keyword2;
 	}
+
 	public String getKeyword3() {
 		return keyword3;
 	}
+
 	public void setKeyword3(String keyword3) {
 		this.keyword3 = keyword3;
 	}
+
 	public String getKeyword4() {
 		return keyword4;
 	}
+
 	public void setKeyword4(String keyword4) {
 		this.keyword4 = keyword4;
 	}
+
 	public String getKeyword5() {
 		return keyword5;
 	}
+
 	public void setKeyword5(String keyword5) {
 		this.keyword5 = keyword5;
 	}
-	public String getKeyword6() {
-		return keyword6;
-	}
-	public void setKeyword6(String keyword6) {
-		this.keyword6 = keyword6;
-	}
-	public String getKeyword7() {
-		return keyword7;
-	}
-	public void setKeyword7(String keyword7) {
-		this.keyword7 = keyword7;
-	}
-	public String getKeyword8() {
-		return keyword8;
-	}
-	public void setKeyword8(String keyword8) {
-		this.keyword8 = keyword8;
-	}
-	public String getKeyword9() {
-		return keyword9;
-	}
-	public void setKeyword9(String keyword9) {
-		this.keyword9 = keyword9;
-	}
-	public String getKeyword10() {
-		return keyword10;
-	}
-	public void setKeyword10(String keyword10) {
-		this.keyword10 = keyword10;
-	}
+
 	public int getKeyword1_freq() {
 		return keyword1_freq;
 	}
+
 	public void setKeyword1_freq(int keyword1_freq) {
 		this.keyword1_freq = keyword1_freq;
 	}
+
 	public int getKeyword2_freq() {
 		return keyword2_freq;
 	}
+
 	public void setKeyword2_freq(int keyword2_freq) {
 		this.keyword2_freq = keyword2_freq;
 	}
+
 	public int getKeyword3_freq() {
 		return keyword3_freq;
 	}
+
 	public void setKeyword3_freq(int keyword3_freq) {
 		this.keyword3_freq = keyword3_freq;
 	}
+
 	public int getKeyword4_freq() {
 		return keyword4_freq;
 	}
+
 	public void setKeyword4_freq(int keyword4_freq) {
 		this.keyword4_freq = keyword4_freq;
 	}
+
 	public int getKeyword5_freq() {
 		return keyword5_freq;
 	}
+
 	public void setKeyword5_freq(int keyword5_freq) {
 		this.keyword5_freq = keyword5_freq;
 	}
-	public int getKeyword6_freq() {
-		return keyword6_freq;
-	}
-	public void setKeyword6_freq(int keyword6_freq) {
-		this.keyword6_freq = keyword6_freq;
-	}
-	public int getKeyword7_freq() {
-		return keyword7_freq;
-	}
-	public void setKeyword7_freq(int keyword7_freq) {
-		this.keyword7_freq = keyword7_freq;
-	}
-	public int getKeyword8_freq() {
-		return keyword8_freq;
-	}
-	public void setKeyword8_freq(int keyword8_freq) {
-		this.keyword8_freq = keyword8_freq;
-	}
-	public int getKeyword9_freq() {
-		return keyword9_freq;
-	}
-	public void setKeyword9_freq(int keyword9_freq) {
-		this.keyword9_freq = keyword9_freq;
-	}
-	public int getKeyword10_freq() {
-		return keyword10_freq;
-	}
-	public void setKeyword10_freq(int keyword10_freq) {
-		this.keyword10_freq = keyword10_freq;
-	}
-	public int getK_total() {
-		return k_total;
-	}
-	public void setK_total(int k_total) {
-		this.k_total = k_total;
-	}
+
 	@Override
 	public String toString() {
 		return "TrendKeyword [k_id=" + k_id + ", k_month=" + k_month + ", keyword1=" + keyword1 + ", keyword2="
 				+ keyword2 + ", keyword3=" + keyword3 + ", keyword4=" + keyword4 + ", keyword5=" + keyword5
-				+ ", keyword6=" + keyword6 + ", keyword7=" + keyword7 + ", keyword8=" + keyword8 + ", keyword9="
-				+ keyword9 + ", keyword10=" + keyword10 + ", keyword1_freq=" + keyword1_freq + ", keyword2_freq="
-				+ keyword2_freq + ", keyword3_freq=" + keyword3_freq + ", keyword4_freq=" + keyword4_freq
-				+ ", keyword5_freq=" + keyword5_freq + ", keyword6_freq=" + keyword6_freq + ", keyword7_freq="
-				+ keyword7_freq + ", keyword8_freq=" + keyword8_freq + ", keyword9_freq=" + keyword9_freq
-				+ ", keyword10_freq=" + keyword10_freq + ", k_total=" + k_total + "]";
+				+ ", keyword1_freq=" + keyword1_freq + ", keyword2_freq=" + keyword2_freq + ", keyword3_freq="
+				+ keyword3_freq + ", keyword4_freq=" + keyword4_freq + ", keyword5_freq=" + keyword5_freq + "]";
 	}
-	
-	
 
 }
