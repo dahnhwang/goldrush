7e983f3722cbf5d0dd098e2dea87a6530332de3c
student <student@M110110>
2018-02-06 10:31:30 +0900

    TrendKeyword - 다현

--- src/dao/ITrendKeywordDao.java
+++ src/dao/ITrendKeywordDao.java
@@ -7,7 +7,7 @@ import model.TrendKeyword;
 
 public interface ITrendKeywordDao {
 
-//	조회하고자 하는 년도를 입력하면 해당 년도에 해당하는 월별 키워드셋을 모두 가져옴
-	public List<TrendKeyword> selectTrendKeywordByYear(HashMap k_year);
-	
+	// 조회하고자 하는 년도를 입력하면 해당 년도에 해당하는 월별 키워드셋을 모두 가져옴
+	public List<TrendKeyword> selectTrendKeywordByYear(HashMap<String, Object> params);
+
 }
