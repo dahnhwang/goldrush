a70f03e88abb07340839d085e4a5a0fbd32e43d0
student <student@M110110>
2018-02-06 10:36:37 +0900

    트렌드 키워드 서비스단 수정 - 다현

--- src/service/ITrendKeywordService.java
+++ src/service/ITrendKeywordService.java
@@ -1,7 +1,10 @@
 package service;
 
+import java.util.HashMap;
+
 public interface ITrendKeywordService {
-	
-	
+
+//	년도를 입력하면 해당 연도에 해당하는 데이터 가져옴
+	public HashMap<String, Object> getTrendKeywordList(String k_year);
 
 }
--- src/service/TrendKeywordService.java
+++ src/service/TrendKeywordService.java
@@ -1,5 +1,13 @@
 package service;
 
+import java.util.HashMap;
+
 public class TrendKeywordService implements ITrendKeywordService {
+
+	@Override
+	public HashMap<String, Object> getTrendKeywordList(String k_year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
