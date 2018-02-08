a1c5f018b06c42eb676af8d14d89e9bdc82c78ec
student <student@M110110>
2018-02-08 18:15:57 +0900

    왕석님 news 부분 빈등록 - 다현

--- src/service/INewsService.java
+++ src/service/INewsService.java
@@ -1,5 +1,18 @@
 package service;
 
+import java.util.List;
+
+import model.News;
+
 public interface INewsService {
 
+	public News getNews(int news_id);
+	
+	public List<News> getNewsList();
+	
+	public List<News> getNewsListSearch(int eng, String Nkeyword);
+	
+	
+	
+	
 }
