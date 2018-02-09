4e28a8011a67357f68196838996057bd2e942557
student <student@M110110>
2018-02-09 11:40:16 +0900

    Factors Month 관련 파일 작성 - 다현

---
+++ src/dao/IFactorsMonthDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import model.FactorsMonth;
+
+public interface IFactorsMonthDao {
+
+	// default로 최근 12개월간 금가격 데이터를 가져옴
+	public List<FactorsMonth> selectRecentGoldPrice(HashMap<String, Object> params);
+
+	// 조회하고자 하는 년을 입력받아 해당 기간의 금가격 데이터를 모두 가져옴
+	public List<FactorsMonth> selectGoldPriceByYear(HashMap<String, Object> params);
+
+}
---
+++ src/service/FactorsMonthService.java
@@ -0,0 +1,22 @@
+package service;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FactorsMonthService implements IFactorsMonthService {
+
+	@Override
+	public HashMap<String, Object> getRecentGoldPriceList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, Object> getGoldPriceListByYear(String k_year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
