514de1965d2f6a58ab56fffba00586abe56ec793
student <student@M110110>
2018-02-13 14:34:32 +0900

    membercontroller 작성 시작 - 다현

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
-	
-	
-	
+
+	@RequestMapping("loginForm.do")
+	public String loginForm() {
+		return "loginForm";
+	}
 
 }
