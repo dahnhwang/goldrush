e45924900173991930bb5cf1493411ae51451803
user <user@Pearl>
2018-02-14 13:47:14 +0900

    aboutForecast.do로 페이지 연결해놓았어요 진주

--- src/controller/ForecastController.java
+++ src/controller/ForecastController.java
@@ -1,8 +1,16 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ForecastController {
+	
+	@RequestMapping("aboutForecast.do")
+	public ModelAndView forecast() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
 
 }
